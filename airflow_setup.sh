fe6ce787e24d1ad208606e487dc36053e71f703f
Nazarii Kvasnytsya <68987909+jackass29@users.noreply.github.com>
2024-02-14 10:23:09 +0200

    Update airflow_setup.sh

--- airflow_setup.sh
+++ airflow_setup.sh
@@ -1,2 +1,18 @@
 sudo apt update
-sudo apt install python3-pip
+
+apt install python3-pip
+apt install python3-venv
+
+python3 -m venv airflow-venv
+
+source airflow-venv/bin/activate
+
+pip install pandas
+pip install apache-airflow
+# if you use AWS EC2 virtual machine
+# pip install s3fs 
+
+# final command
+airflow_standalone
+
+
